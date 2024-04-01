77cfeadc18fb3c55ee11a7ab7b46ad754cdda8fd
KelvGpd <kelvgpd@gmail.com>
2024-04-01 18:47:40 -0400

    Q4 algo1

--- Algo_Tp3/Q4 Labyrinthe/labyrinth_generator_creator.py
+++ Algo_Tp3/Q4 Labyrinthe/labyrinth_generator_creator.py
@@ -4,6 +4,7 @@ import random
 cell_size = 10 #mm
 wall_height = 10 #mm
 wall_thickness = 1 #mm
+cell_amount = 20 #number of cells going both sides of the square
 
 strategy_choice = 1
 
@@ -17,10 +18,51 @@ class Strategy :
     def DoSomething(self):
         print("Do Something")
 
+class Coords:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+class Wall:
+    def __init__(self, coords1, coords2) -> None:
+        self.source = coords1
+        self.dest = coords2
+
 class Algorithm1(Strategy) :
+
     def Apply(self):
         #super().Apply()
         print("Applying Algorithm1")
+        #Iterative randomized Prim's algorithm
+        cells = [[0] * cell_amount for _ in range(cell_amount)]
+        walls = []
+        #Start with a grid full of walls. or no passages
+        passages = []
+        #Pick a cell, mark it as part of the maze.
+        startCoords = Coords(random(cell_size), random(cell_size))
+        cells[startCoords.x][startCoords.y] = 1
+        #Add the walls of the cell to the wall list
+        walls.append(self.getWallsOfCell(cells, startCoords))
+        #While there are walls in the list
+        while(walls):
+            #Pick a random wall from the list.
+            wall = walls[0]
+            #If only one of the cells that the wall divides is visited, then:
+            if (cells[wall.dest.x][wall.dest.y] == 0):
+                #Make the wall a passage and mark the unvisited cell as part of the maze.
+                passages.append(wall)
+                cells[wall.dest.x][wall.dest.y] == 1
+                #Add the neighboring walls of the cell to the wall list.
+                walls.append(self.getWallsOfCell(cells, wall.dest))
+            #Remove the wall from the list.
+            walls.pop()
+    
+    def getWallsOfCell(cells, coords):
+        l = Wall(coords, Coords(coords.x - 1, coords.y)) if (coords.x - 1 >= 0) else None
+        r = Wall(coords, Coords(coords.x + 1, coords.y)) if (coords.x + 1 <= cell_amount - 1) else None
+        u = Wall(coords, Coords(coords.x, coords.y + 1)) if (coords.y - 1 >= 0) else None
+        d = Wall(coords, Coords(coords.x, coords.y - 1)) if (coords.y + 1 <= cell_amount - 1) else None
+        return l, r, u, d
 
 class Algorithm2(Strategy) :
 
@@ -42,6 +84,7 @@ class Generator() :
         self.strategy.DoSomething()
 
 class Creator() :
+    #generate .scad file
     def __init__(self):
         pass
 
